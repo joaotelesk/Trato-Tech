06bf970df5234ac8951e1a2a934894495d63e2de
Joao Teles <joaojoseteles@gmail.com>
2022-11-22 17:16:09 -0300

    :sparkles: feat: utilizaçao do react-hook-form no formulario da pagina anuncie

--- src/pages/Anuncie/index.jsx
+++ src/pages/Anuncie/index.jsx
@@ -2,25 +2,39 @@ import Button from "components/Button";
 import Header from "components/Header";
 import { useSelector } from "react-redux";
 import styles from "./Anuncie.module.scss";
+import { useForm } from "react-hook-form";
 
 export default function Anuncie() {
   const categorias = useSelector((state) =>
     state.categorias.map(({ nome, id }) => ({ nome, id }))
   );
+  const { register, handleSubmit } = useForm();
+  function cadastrar(parametro) {
+    console.log(parametro);
+  }
   return (
     <div className={styles.container}>
       <Header
         titulo="Anuncie aqui"
         descricao="Anuncie seu produto no melhor saite do Brasil"
       />
-      <form className={styles.formulario}>
-        <input placeholder="Nome do produto" alt="nome do produto" />
-        <input placeholder="Descrição do produto" alt="Descrição do produto" />
+      <form className={styles.formulario} onSubmit={handleSubmit(cadastrar)}>
         <input
+          {...register("nome")}
+          placeholder="Nome do produto"
+          alt="nome do produto"
+        />
+        <input
+          {...register("descricao")}
+          placeholder="Descrição do produto"
+          alt="Descrição do produto"
+        />
+        <input
+          {...register("imagem")}
           placeholder="Url da imagem do produto"
           alt="Url da imagem do produto"
         />
-        <select>
+        <select {...register("categoria")}>
           <option value="" disabled>
             Selecione a categorias
           </option>
@@ -30,7 +44,11 @@ export default function Anuncie() {
             </option>
           ))}
         </select>
-        <input type="number" placeholder="Preço do produto" />
+        <input
+          {...register("preco")}
+          type="number"
+          placeholder="Preço do produto"
+        />
         <Button type="submite">Cadastrar produto</Button>
       </form>
     </div>
