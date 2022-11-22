23b71c44ae105a57e0e142d898d7b356a35c39d3
Joao Teles <joaojoseteles@gmail.com>
2022-11-22 16:35:35 -0300

    :sparkles: feat: Pagina Anuncie

--- src/pages/Anuncie/index.jsx
+++ src/pages/Anuncie/index.jsx
@@ -1,3 +1,38 @@
+import Button from "components/Button";
+import Header from "components/Header";
+import { useSelector } from "react-redux";
+import styles from "./Anuncie.module.scss";
+
 export default function Anuncie() {
-  return <div>anuncie aqui</div>;
+  const categorias = useSelector((state) =>
+    state.categorias.map(({ nome, id }) => ({ nome, id }))
+  );
+  return (
+    <div className={styles.container}>
+      <Header
+        titulo="Anuncie aqui"
+        descricao="Anuncie seu produto no melhor saite do Brasil"
+      />
+      <form className={styles.formulario}>
+        <input placeholder="Nome do produto" alt="nome do produto" />
+        <input placeholder="Descrição do produto" alt="Descrição do produto" />
+        <input
+          placeholder="Url da imagem do produto"
+          alt="Url da imagem do produto"
+        />
+        <select>
+          <option value="" disabled>
+            Selecione a categorias
+          </option>
+          {categorias.map((categoria) => (
+            <option key={categoria.id} value={categoria.id}>
+              {categoria.nome}
+            </option>
+          ))}
+        </select>
+        <input type="number" placeholder="Preço do produto" />
+        <Button type="submite">Cadastrar produto</Button>
+      </form>
+    </div>
+  );
 }
